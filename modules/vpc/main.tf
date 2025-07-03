c131bef2610dc226fa9bf4fd4d7562c6f5614d72
sudhir <sudhir.suvarna@mq.edu.au>
2025-07-04 08:43:29 +1000

    push to git main

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,23 @@
 provider "aws" {
   region = var.region
-  access_key = "AKIAWYIYEHRNL3KVS6NL"  # Your AWS Access Key
-  secret_key = "Ztwtqp0xis9CE4UcPvM3r7WIwuSLet/25upCYfVN"  # Your AWS Secret Key
+  # access_key = "AKIAWYIYEHRNL3KVS6NL"  # Your AWS Access Key
+  # secret_key = "Ztwtqp0xis9CE4UcPvM3r7WIwuSLet/25upCYfVN"  # Your AWS Secret Key
+}
+terraform {
+  backend "remote" {
+    organization = "sidterraorg"
+
+    workspaces {
+      name = "acmtesting"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 resource "aws_vpc" "main" {
