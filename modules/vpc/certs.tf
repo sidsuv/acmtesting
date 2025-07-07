20c2454974682708f5080b4148538f27e4b4703e
sudhir <sudhir.suvarna@mq.edu.au>
2025-07-07 11:42:26 +1000

    adding cert to the domain

--- modules/vpc/certs.tf
+++ modules/vpc/certs.tf
@@ -14,19 +14,41 @@ resource "aws_acm_certificate" "cert" {
 }
 
 # 2️⃣ Create the Route 53 DNS validation record
+# resource "aws_route53_record" "cert_validation" {
+#   name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+#   type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+#   zone_id = "Z03054613MGI9EV9H1546"
+#   ttl     = 60
+#   records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+# }
+
+# # 3️⃣ Complete the validation step
+# resource "aws_acm_certificate_validation" "cert_validation_complete" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+# }
+
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   zone_id = "Z03054613MGI9EV9H1546"
+  name    = each.value.name
+  type    = each.value.type
   ttl     = 60
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  records = [each.value.record]
 }
 
-# 3️⃣ Complete the validation step
 resource "aws_acm_certificate_validation" "cert_validation_complete" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
+
 
 # 4️⃣ Output the cert ARN
 output "certificate_arn" {
